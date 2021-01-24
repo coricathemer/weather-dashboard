a1f3a3147d13c167b434ebef4dfc1e71d4bcb8be
Cori Cathemer <coricathemer@gmail.com>
2021-01-24 15:49:05 -0700

    updated html file

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,15 +4,17 @@ var searchTerm = "Tucson";
 // displays current and future conditions for searched city and save that search to local storage
 var APIkey = "f28e06375945149aa9a3231cdaaa0e87"
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${APIkey}
 fetch (
-  `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${APIkey}`
+  `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${APIkey}`
 )
 .then(function(response) {
   return response.json();
 })
 .then(function(response) {
   console.log(response);
+
+  var forecastDay1Date = response.list[0].dt_txt;
 })
 
 
