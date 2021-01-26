715f4fbfcd7e01a1faa4a1abe0aa95235bff0a34
coricathemer <coricathemer@gmail.com>
2021-01-25 17:26:03 -0700

    restructured files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-  var cityInputEl = document.querySelector("#search-input");
+  // var cityInputEl = document.querySelector("#search-input");
 
   // user search bar function for all us cities 
-  var searchTerm = cityInputEl.nodeValue.trim();
+  var searchTerm = "searchTerm"
 
-  if (searchTerm) {
-    getCityWeather(searchTerm);
-  } else {
-    alert("Please enter a city name.")
-  };
+  // if (searchTerm) {
+  //   getCityWeather(searchTerm);
+  // } else {
+  //   alert("Please enter a city name.")
+  // };
   
 
  
   // grab searchTerm from HTML input tag
-  var getCityWeather = function(city) {
-    let APIkey = "f28e06375945149aa9a3231cdaaa0e87" + city;
+  // var getCityWeather = function(city) {
+    let APIkey = "f28e06375945149aa9a3231cdaaa0e87"
   
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${APIkey}` )
     .then(function(response) {
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
     // put past searches into localStorage.
     });
-  }
+  
 
   // render info from api on the page 
 
